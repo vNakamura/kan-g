f8b33e4cbfc8328dcc238bc16e8d8fa5162ab1e4
vNakamura <git@vnaka.dev>
2023-11-23 17:18:42 +0900

    allow access to function from any origin

--- functions/c/[char].js
+++ functions/c/[char].js
@@ -1,21 +1,30 @@
-import kvgindex from '../../public/kg-index.json'
+import kvgindex from "../../public/kg-index.json"
 
 function notFound(body = "Not Found") {
-	return new Response(body, { status: 404 })
+  return new Response(body, { status: 404 })
 }
 
-export function onRequest({request, params}) {
-	console.log(request)
-	const char = params.char
-	if(!char) return notFound()
-	
-	const decodedChar = decodeURIComponent(char)
-	if(!decodedChar) return notFound()
-	
-	const file = kvgindex[decodedChar]
-	if(!file) return notFound()
-	
-	const url = new URL(request.url)
-	const newPath = `${url.origin}/k/${file}`
-	return Response.redirect(newPath, 301);
+export function onRequest({ request, params }) {
+  console.log(request)
+  const char = params.char
+  if (!char) return notFound()
+
+  const decodedChar = decodeURIComponent(char)
+  if (!decodedChar) return notFound()
+
+  const file = kvgindex[decodedChar]
+  if (!file) return notFound()
+
+  const url = new URL(request.url)
+  const newPath = `${url.origin}/k/${file}`
+
+  return new Response(null, {
+    status: 301,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*",
+      Location: newPath,
+      "Access-Control-Max-Age": "86400",
+    },
+  })
 }
