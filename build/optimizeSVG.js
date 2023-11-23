2c76e3401a5ddd6fdd37afb39c5d9936e0fc52b8
vNakamura <git@vnaka.dev>
2023-11-23 13:08:27 +0900

    remove dimension attributes

--- build/optimizeSVG.js
+++ build/optimizeSVG.js
@@ -1,30 +1,26 @@
-import cliProgress from 'cli-progress'
-import kanjivgIndex from '../kanjivg/kvg-index.json'
-import svgo from 'svgo'
+import cliProgress from "cli-progress"
+import kanjivgIndex from "../kanjivg/kvg-index.json"
+import svgo from "svgo"
 
-const kanjivgPath = './kanjivg/kanji'
-const outputPath = './public/k'
-const indexOutput = './public/kg-index.json'
-
+const kanjivgPath = "./kanjivg/kanji"
+const outputPath = "./public/k"
+const indexOutput = "./public/kg-index.json"
 
 const reducedIndex = Object.fromEntries(
-  Object.entries(kanjivgIndex).map(
-    ([char, files]) => {
-      const file = files.findLast((name) => !name.includes("-"))
-        || files.at(-1)
-      return [char, file]
-    }
-  )
+  Object.entries(kanjivgIndex).map(([char, files]) => {
+    const file = files.findLast((name) => !name.includes("-")) || files.at(-1)
+    return [char, file]
+  })
 )
 
 const filenames = Object.values(reducedIndex)
 const bar = new cliProgress.SingleBar({
-  etaBuffer: 300
+  etaBuffer: 300,
 })
 
 console.log("Optimizing SVG files:")
 bar.start(filenames.length)
-for(const filename of filenames) {
+for (const filename of filenames) {
   await proccessFile(filename)
   bar.increment()
 }
@@ -37,29 +33,30 @@ console.log("Process finished.")
 async function proccessFile(filename) {
   const file = Bun.file(`${kanjivgPath}/${filename}`)
   const originalSVG = (await file.text())
-      // remove kvg: namespaced attributes wich breaks svgo parser
+    // remove kvg: namespaced attributes wich breaks svgo parser
     .replace(/\s*kvg:\w+="[^"]*"/g, "")
 
-      // use classes intead of ids for the Paths and Numbers groups
+    // use classes intead of ids for the Paths and Numbers groups
     .replace(/id="kvg:Stroke(Paths|Numbers)\w*"/g, `class="kg$1"`)
 
   const optimizedSVG = svgo.optimize(originalSVG, {
     plugins: [
       {
-        name: 'preset-default',
+        name: "preset-default",
         params: {
           overrides: {
             convertColors: false,
             removeViewBox: false,
-            mergePaths: false
-          }
-        }
+            mergePaths: false,
+          },
+        },
       },
+      "removeDimensions",
       {
         name: "addClassesToSVGElement",
         params: {
-          className: "kan-g"
-        }
+          className: "kan-g",
+        },
       },
       "convertStyleToAttrs",
       {
@@ -70,9 +67,9 @@ async function proccessFile(filename) {
           rgb2hex: true,
           shorthex: true,
           shortname: true,
-        }
+        },
       },
-    ]
+    ],
   })
 
   await Bun.write(`${outputPath}/${filename}`, optimizedSVG.data)
